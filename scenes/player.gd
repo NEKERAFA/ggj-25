ab7ea5b60470ddbc3c3d99cb3a557f1b3fc50e25
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2025-01-25 00:11:11 +0100

    Added jump mechanic

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,29 +1,47 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED := 5.0
+const JUMP_VELOCITY := 4.5
 
 
-func _physics_process(delta):
+@export var start_movement: bool = false
+
+var _mouse_entered: bool = false
+
+
+func _input(event):
+	# Handle jump.
+	if (
+			not _mouse_entered
+			and velocity.x > 0
+			and is_on_floor()
+			and (event is InputEventMouseButton)
+	):
+		var mouse_event = event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
+			velocity.y = JUMP_VELOCITY
+
+
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-	velocity.x = SPEED
 
 	move_and_slide()
+
+
+func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	# Move player to left
+	if event is InputEventMouseButton:
+		var mouse_event = event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
+			velocity.x = SPEED
+
+
+func _on_mouse_exited():
+	_mouse_entered = false
+
+
+func _on_mouse_entered():
+	_mouse_entered = true
