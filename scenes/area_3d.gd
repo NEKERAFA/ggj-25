092b9aafbe92672c1381463a7bcf08cba7209bac
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2025-01-25 13:35:45 +0100

    Word rotation

--- scenes/area_3d.gd
+++ scenes/area_3d.gd
@@ -1,5 +1,16 @@
 extends Area3D
+class_name MouseGrabArea3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Sprite3D.visible = monitoring
+
+
+func start_grab() -> void:
+	monitoring = true
+	$CollisionShape3D.disabled = false
+
+
+func release_grab() -> void:
+	monitoring = false
+	$CollisionShape3D.disabled = true
--- scripts/word.gd
+++ scripts/word.gd
@@ -1,10 +1,15 @@
 extends AnimatableBody3D
 
 
+const GRABBED_WAIT = 0.2
+
+
 var _grabbed: bool = false
+var _gabbed_time: float = 0.0
+var _moved: bool = false
 
 
-@export var mouse_area: Area3D = null
+@export var mouse_area: MouseGrabArea3D = null
 
 
 func _ready() -> void:
@@ -12,36 +17,56 @@ func _ready() -> void:
 		mouse_area.input_event.connect(_on_area3d_input_event)
 
 
+func _process(delta: float) -> void:
+	if _grabbed and not _moved:
+		_gabbed_time += delta
+
+		if _gabbed_time >= GRABBED_WAIT:
+			# Rotate and reset
+			rotate_z(deg_to_rad(90))
+			_reset_grab()
+
+
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	# Add grab state
 	if event is InputEventMouseButton:
 		var mouse_event = event as InputEventMouseButton
 		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-			_grabbed = true
-			mouse_area.monitoring = true
-			$CollisionShape3D.disabled = true
-			
-			for child in mouse_area.get_children():
-				if child is CollisionShape3D:
-					(child as CollisionShape3D).disabled = false
+			_start_grab()
 
 
 func _on_area3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	pass
-	# Reset grab state
 	if event is InputEventMouseButton:
 		var mouse_event = event as InputEventMouseButton
 		if mouse_event.button_index == MOUSE_BUTTON_LEFT and not mouse_event.pressed:
-			_grabbed = false
-			mouse_area.monitoring = false
-			$CollisionShape3D.disabled = false
+			# Rotate
+			if not _moved:
+				rotate_z(deg_to_rad(90))
 			
-			for child in mouse_area.get_children():
-				if child is CollisionShape3D:
-					(child as CollisionShape3D).disabled = false
+			# Reset grab state
+			_reset_grab()
 	
 	# Move grab state
 	if event is InputEventMouseMotion:
 		var mouse_event = event as InputEventMouseMotion
-		if _grabbed:
+		if _grabbed and (_moved or global_position.distance_to(event_position) > 0.1):
+			# Update position
 			position = Vector3(event_position.x, event_position.y, 0)
+			if not _moved:
+				_moved = true
+
+
+func _start_grab():
+	_grabbed = true
+	_gabbed_time = 0.0
+	_moved = false
+	$CollisionShape3D.disabled = true
+	mouse_area.start_grab()
+
+
+func _reset_grab():
+	_grabbed = false
+	_gabbed_time = 0.0
+	_moved = false
+	$CollisionShape3D.disabled = false
+	mouse_area.release_grab()
