b1211380bd72870201645bf906634e6f6e1d5c29
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2025-01-25 09:38:02 +0100

    Added coyote time

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,32 +1,56 @@
 extends CharacterBody3D
 
 
-const SPEED := 5.0
+const SPEED := 4.0
 const JUMP_VELOCITY := 4.5
 
 
 @export var start_movement: bool = false
 
-var _mouse_entered: bool = false
+@onready var _coyote_timer = $CoyoteTimer
 
+var _mouse_entering: bool = false
+var _jumping: bool = false
+var _is_coyote: bool = false
+var _is_coyote_finished: bool = true
 
 func _input(event):
 	# Handle jump.
-	if (
-			not _mouse_entered
-			and velocity.x > 0
-			and is_on_floor()
-			and (event is InputEventMouseButton)
-	):
+	if event is InputEventMouseButton:
 		var mouse_event = event as InputEventMouseButton
-		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-			velocity.y = JUMP_VELOCITY
+		if (
+			mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed
+			and not _mouse_entering and velocity.x > 0 # x > 0? player is moving
+			and not _jumping
+		):
+			_jumping = true
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Make jump
+	if _jumping and velocity.y == 0 and (is_on_floor() or (_coyote_timer and not _is_coyote_finished)):
+		velocity.y = JUMP_VELOCITY
+	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		# Add coyote time
+		if not _jumping and not _is_coyote:
+			_is_coyote = true
+			_is_coyote_finished = false
+			$CoyoteTimer.start()
+		
+		# Add the gravity.
+		elif _jumping or (_is_coyote and _is_coyote_finished):
+			velocity += get_gravity() * delta
+	else:
+		# Reset jump
+		if _jumping:
+			_jumping = false
+		
+		# Reset coyote time
+		if _is_coyote:
+			_is_coyote = false
+			_is_coyote_finished = true
+			$CoyoteTimer.stop()
 
 	move_and_slide()
 
@@ -35,13 +59,18 @@ func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, n
 	# Move player to left
 	if event is InputEventMouseButton:
 		var mouse_event = event as InputEventMouseButton
+		# Start movement
 		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
 			velocity.x = SPEED
 
 
 func _on_mouse_exited():
-	_mouse_entered = false
+	_mouse_entering = false
 
 
 func _on_mouse_entered():
-	_mouse_entered = true
+	_mouse_entering = true
+
+
+func _on_coyote_timeout():
+	_is_coyote_finished = true
